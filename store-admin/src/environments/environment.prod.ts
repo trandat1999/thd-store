5e5f107f39f52fa81f6912cd336e5189ffdde23e
DatNuclear <datth.revotech@gmail.com>
2024-08-22 16:09:06 +0700

    login with google

--- store-admin/src/environments/environment.prod.ts
+++ store-admin/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  fileConfig: "assets/config/config.prod.json"
+  fileConfig: "assets/config/config.prod.json",
+  googleClientId: "383648750558-siijikq5679t86hvigi8q4b78k26l1op.apps.googleusercontent.com"
 };
